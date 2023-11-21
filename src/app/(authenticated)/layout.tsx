3021d3f0a7d280f22266201a9fc8e168c16130ca
Alexis Ken Alvarez <alexisken1432@gmail.com>
2023-11-21 15:46:44 +0800

    removed private route

--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -1,17 +1,10 @@
-import { authOptions } from "@/server/auth";
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
 
 export default async function authLayout({
   children, // will be a page or nested layout
 }: {
   children: React.ReactNode;
 }) {
-  const session = await getServerSession(authOptions);
 
-  if (!session) {
-    redirect('/signin')
-  }
 
   return <section>{children}</section>;
 }
