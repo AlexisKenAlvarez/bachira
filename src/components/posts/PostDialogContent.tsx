b02ca502f203f4c9f14daa580247c23d0a91f124
Alexis Ken Alvarez <alexisken1432@gmail.com>
2024-01-10 09:51:47 +0800

    test

--- src/components/posts/PostDialogContent.tsx
+++ src/components/posts/PostDialogContent.tsx
@@ -116,7 +116,7 @@ const PostDialogContent = ({
     return (
       <div className="flex gap-2 py-1">
         <Avatar className="h-8 w-8">
-          <AvatarImage src={suggestion.image} className="object-cover" />
+          {/* <AvatarImage src={suggestion.image} className="object-cover" /> */}
           <AvatarFallback>
             <Skeleton className="h-full w-full rounded-full" />
           </AvatarFallback>
